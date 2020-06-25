796397dbf08245cd65cf51e92dd178b2a368d55d
shreyamisale88 <62130728+shreyamisale88@users.noreply.github.com>
2020-06-25 13:59:40 +0530

    first commit

---
+++ project-master/README.md
@@ -0,0 +1,4 @@
+# React Portfolio
+
+A portfolio website.
+
---
+++ project-master/src/constants/About.js
@@ -0,0 +1,5 @@
+export const about = {
+    heading: "About Me",
+    "quote": "The best way to predict your future is to create it.",
+    "bio": "A student at MGM's College of Engineering,Nanded. \nA coder by passion, a tech enthusiast,learning new languages and\nupgrading in the technical world.Love music,singing,travelling.\n\n\n*Contact Details*\nEmail Id :-  bhaktipatil246@gmail.com\nContact Number :-  9881982844"
+};
